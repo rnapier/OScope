0e0d19ce540dea0838473248621aadd1b5aa4aea
Rob Napier <robnapier@gmail.com>
2015-02-08 12:40:42 -0500

    Move ScopeViewController and SignalVisualizerView over to Visualizer

--- OScope/ScopeViewController.swift
+++ OScope/ScopeViewController.swift
@@ -17,66 +17,65 @@ enum VisualizerDomain {
 
 class ScopeViewController: UIViewController {
 
-  @IBOutlet weak var signalVisualizerView : SignalVisualizerView!
-  @IBOutlet weak var yScaleKnob: KnobControl!
-  @IBOutlet weak var yScaleAutoButton: HighlightButton!
-  @IBOutlet weak var xScaleKnob: KnobControl!
-  @IBOutlet weak var domainSwitch: UISwitch!
+  @IBOutlet weak var signalVisualizerView : SignalVisualizerView?
+  @IBOutlet weak var yScaleKnob: KnobControl?
+  @IBOutlet weak var yScaleAutoButton: HighlightButton?
+  @IBOutlet weak var xScaleKnob: KnobControl?
+  @IBOutlet weak var domainSwitch: UISwitch?
 
   var yScale: VisualizerScale  {
-  get { return self.signalVisualizerView.voltsPerDiv }
+  get { return self.signalVisualizerView?.voltsPerDiv ?? .Automatic }
   set {
-    self.signalVisualizerView.voltsPerDiv = newValue
+    self.signalVisualizerView?.voltsPerDiv = newValue
     self.updateYScaleControls(animated: true)
   }
   }
 
   var source : SignalSource? {
-  get { return self.signalVisualizerView.source }
+  get { return self.signalVisualizerView?.source }
   set(newValue) {
-    self.signalVisualizerView.source = newValue
+    self.signalVisualizerView?.source = newValue
     self.updateYScaleControls(animated:false)
   }
   }
 
   var domain : VisualizerDomain {
-  get { return self.signalVisualizerView.domain }
-  set { self.signalVisualizerView.domain = newValue }
+  get { return self.signalVisualizerView?.domain ?? .Time }
+  set { self.signalVisualizerView?.domain = newValue }
   }
 
   override func viewDidLoad() {
-    self.yScaleKnob.minimumValue = 0.1
-    self.yScaleKnob.maximumValue = 10
+    self.yScaleKnob?.minimumValue = 0.01
+    self.yScaleKnob?.maximumValue = 0.5
     self.updateYScaleControls(animated: false)
   }
 
   func updateYScaleControls(#animated: Bool) {
     switch self.yScale {
     case .Absolute(_):
-      self.yScaleAutoButton.value = false
-      self.yScaleKnob.enabled = true
-      self.yScaleKnob.tintColor = UIColor.blueColor()
+      self.yScaleAutoButton?.value = false
+      self.yScaleKnob?.enabled = true
+      self.yScaleKnob?.tintColor = UIColor.blueColor()
     case .Automatic:
-      self.yScaleAutoButton.value = true
-      self.yScaleKnob.enabled = false
-      self.yScaleKnob.tintColor = UIColor.grayColor().colorWithAlphaComponent(0.3)
+      self.yScaleAutoButton?.value = true
+      self.yScaleKnob?.enabled = false
+      self.yScaleKnob?.tintColor = UIColor.grayColor().colorWithAlphaComponent(0.3)
 
-      // FIXME: Get scale back
-//      if let visualizer = signalVisualizerView.visualizer {
-//        self.yScaleKnob.setValue(CGFloat(visualizer.automaticYScale), animated: animated)
-//      }
-//      else {
-//        self.yScaleKnob.setValue(0, animated: animated)
-//      }
+      if let visualizer = signalVisualizerView?.visualizer {
+        self.yScaleKnob?.setValue(CGFloat(visualizer.automaticYScale), animated: animated)
+      }
+      else {
+        self.yScaleKnob?.setValue(0, animated: animated)
+      }
     }
   }
   @IBAction func yScaleChanged(sender: KnobControl) {
-    self.yScale = .Absolute(Float(self.yScaleKnob.value))
+    self.yScale = .Absolute(Float(self.yScaleKnob?.value ?? 0))
   }
 
   @IBAction func performYScaleAutoButton(sender: HighlightButton) {
-    if self.yScaleAutoButton.value {
-      self.yScale = .Absolute(Float(self.yScaleKnob.value))
+    if self.yScaleAutoButton?.value ?? false {
+      self.yScale = .Absolute(Float(self.yScaleKnob?.value ?? 0))
     } else {
       self.yScale = .Automatic
     }
@@ -84,7 +83,7 @@ class ScopeViewController: UIViewController {
   }
 
   @IBAction func performDomainSwitch(sender: AnyObject) {
-    self.domain = self.domainSwitch.on ? .Frequency : .Time
+    self.domain = (self.domainSwitch?.on ?? true) ? .Frequency : .Time
   }
 
   @IBAction func xScaleChanged(sender: KnobControl) {
--- OScope/SignalVisualizerView.swift
+++ OScope/SignalVisualizerView.swift
@@ -12,194 +12,169 @@ import SignalKit
 
 class SignalVisualizerView: UIView {
 
-  private let signalLayer = SignalLayer()
-  private let axesLayer = CAShapeLayer()
-  private let gridLayer = CAShapeLayer()
+    private let signalLayer = SignalLayer()
+    private let axesLayer = CAShapeLayer()
+    private let gridLayer = CAShapeLayer()
 
-  let verticalDivisions = 8
-  let horizontalDivisions = 12
+    let verticalDivisions = 8
+    let horizontalDivisions = 12
 
-  private func setup() {
-    self.clipsToBounds = true
+    private func setup() {
+        self.clipsToBounds = true
 
-    layer.addSublayer(self.signalLayer)
+        layer.addSublayer(self.signalLayer)
 
-    self.axesLayer.frame = self.bounds
-    self.axesLayer.path = self.axesPath().CGPath
-    self.axesLayer.strokeColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.66).CGColor
-    self.axesLayer.lineWidth = 3
-    self.layer.addSublayer(self.axesLayer)
+        self.axesLayer.frame = self.bounds
+        self.axesLayer.path = self.axesPath().CGPath
+        self.axesLayer.strokeColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.66).CGColor
+        self.axesLayer.lineWidth = 3
+        self.layer.addSublayer(self.axesLayer)
 
-    self.gridLayer.frame = self.bounds
-    self.gridLayer.path = self.gridPath().CGPath
-    self.gridLayer.strokeColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.33).CGColor
-    self.gridLayer.contentsGravity = kCAGravityCenter
-    self.layer.addSublayer(self.gridLayer)
-  }
-
-  required init(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    self.setup()
-  }
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    self.setup()
-  }
-
-  override var bounds : CGRect {
-    didSet {
-      self.axesLayer.frame = self.bounds
-      self.axesLayer.path = self.axesPath().CGPath
-      self.gridLayer.frame = self.bounds
-      self.gridLayer.path = self.gridPath().CGPath
-      self.updateVisualizer()
+        self.gridLayer.frame = self.bounds
+        self.gridLayer.path = self.gridPath().CGPath
+        self.gridLayer.strokeColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 0.33).CGColor
+        self.gridLayer.contentsGravity = kCAGravityCenter
+        self.layer.addSublayer(self.gridLayer)
     }
-  }
 
-  override func layoutSublayersOfLayer(layer: CALayer!) {
-    let bounds = self.layer.bounds
-    let height = CGRectGetHeight(bounds)
-    self.signalLayer.bounds = CGRectMake(0, -height/2.0, CGRectGetWidth(bounds), height)
-    self.signalLayer.position = CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds))
-  }
-
-  var visualizer : Visualizer? {
-    didSet { self.setNeedsDisplay() }
-  }
-
-  var source : SignalSource? {
-    didSet {
-      self.updateVisualizer()
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
     }
-  }
 
-  private func calculateAutomaticYScale(#values:[CGFloat]) -> CGFloat {
-    return 1/values.map(abs).reduce(CGFloat(FLT_EPSILON), combine: max)
-  }
-
-  var voltsPerDiv : VisualizerScale = .Automatic {
-    didSet {
-      if let source = self.source {
-        let scale: CGFloat = {
-          switch self.voltsPerDiv {
-          case .Absolute(let value):
-            return CGFloat(value) * CGFloat(self.verticalDivisions)
-          case .Automatic:
-            return CGFloat(self.verticalDivisions)
-          }
-          }()
-        let t = CGAffineTransformMakeScale(1, -scale)
-        let p = self.waveform?.path.copy() as UIBezierPath
-        p.applyTransform(t)
-        self.signalLayer.path = p.CGPath
-      }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
     }
-  }
 
-  var secondsPerDiv : SignalTime = 1 * Millisecond {
-    didSet {
-      self.updateVisualizer()
-    }
-  }
-
-  var domain : VisualizerDomain = .Time {
-    didSet {
-      self.updateVisualizer()
-    }
-  }
-
-  private var waveform: Waveform?
-
-  func updateVisualizer() {
-    if let source = self.source {
-      let times = SignalSampleTimes(
-        start: 0*Second,
-        end: self.secondsPerDiv * self.horizontalDivisions,
-        samples: Int(ceil(CGRectGetWidth(self.bounds) + 1)))
-      self.waveform = {
-        switch self.domain {
-        case .Time:
-          return SignalWaveform(source: source, sampleTimes: times)
-        case .Frequency:
-          return SpectrumWaveform(source: source, sampleTimes: times)
+    override var bounds : CGRect {
+        didSet {
+            self.axesLayer.frame = self.bounds
+            self.axesLayer.path = self.axesPath().CGPath
+            self.gridLayer.frame = self.bounds
+            self.gridLayer.path = self.gridPath().CGPath
+            self.updateVisualizer()
         }
-        }()
-      self.signalLayer.path = self.waveform?.path.CGPath
-    }
-  }
-
-
-  func gridPath() -> UIBezierPath {
-    let path = UIBezierPath()
-
-    let vertSpacing = CGRectGetHeight(self.bounds) / CGFloat(self.verticalDivisions)
-    let minX = CGRectGetMinX(self.bounds)
-    let maxX = CGRectGetMaxX(self.bounds)
-    for i in 1 ..< self.verticalDivisions {
-      let y = vertSpacing * CGFloat(i)
-      path.moveToPoint(CGPointMake(minX, y))
-      path.addLineToPoint(CGPointMake(maxX, y))
     }
 
-    let horizSpecing = CGRectGetWidth(self.bounds) / CGFloat(self.horizontalDivisions)
-    let minY = CGRectGetMinY(self.bounds)
-    let maxY = CGRectGetMaxY(self.bounds)
-    for i in 1 ..< self.horizontalDivisions {
-      let x = horizSpecing * CGFloat(i)
-      path.moveToPoint(CGPointMake(x, minY))
-      path.addLineToPoint(CGPointMake(x, maxY))
+    var visualizer : Visualizer? {
+        didSet { self.setNeedsDisplay() }
     }
-    return path
-  }
 
-  func axesPath() -> UIBezierPath {
-    let path = UIBezierPath()
-    path.moveToPoint(CGPointMake(CGRectGetMidX(bounds), CGRectGetMinY(bounds)))
-    path.addLineToPoint(CGPointMake(CGRectGetMidX(bounds), CGRectGetMaxY(bounds)))
+    var source : SignalSource? {
+        didSet {
+            self.updateVisualizer()
+        }
+    }
 
-    path.moveToPoint(CGPointMake(CGRectGetMinX(bounds), CGRectGetMidY(bounds)))
-    path.addLineToPoint(CGPointMake(CGRectGetMaxX(bounds), CGRectGetMidY(bounds)))
+    var voltsPerDiv : VisualizerScale = .Automatic {
+        didSet {
+            self.updateVisualizer()
+        }
+    }
 
-    path.lineWidth = 10
+    var secondsPerDiv : SignalTime = 1 * Millisecond {
+        didSet {
+            self.updateVisualizer()
+        }
+    }
 
-    return path
-  }
+    var domain : VisualizerDomain = .Time {
+        didSet {
+            self.updateVisualizer()
+        }
+    }
+
+    func updateVisualizer() {
+        if let source = self.source {
+            let times = SignalSampleTimes(
+                start: 0*Second,
+                end: self.secondsPerDiv * self.horizontalDivisions,
+                samples: Int(ceil(CGRectGetWidth(self.bounds) + 1)))
+            let waveform: Waveform = {
+                switch self.domain {
+                case .Time:
+                    return SignalWaveform(source: source, sampleTimes: times)
+                case .Frequency:
+                    return SpectrumWaveform(source: source, sampleTimes: times)
+                }
+
+                }()
+            self.visualizer = Visualizer(waveform: waveform, frame: self.bounds, yScale: self.voltsPerDiv)
+            self.signalLayer.path = self.visualizer?.path.CGPath
+        }
+    }
+
+    func gridPath() -> UIBezierPath {
+        let path = UIBezierPath()
+
+        let vertSpacing = CGRectGetHeight(self.bounds) / CGFloat(self.verticalDivisions)
+        let minX = CGRectGetMinX(self.bounds)
+        let maxX = CGRectGetMaxX(self.bounds)
+        for i in 1 ..< self.verticalDivisions {
+            let y = vertSpacing * CGFloat(i)
+            path.moveToPoint(CGPointMake(minX, y))
+            path.addLineToPoint(CGPointMake(maxX, y))
+        }
+
+        let horizSpecing = CGRectGetWidth(self.bounds) / CGFloat(self.horizontalDivisions)
+        let minY = CGRectGetMinY(self.bounds)
+        let maxY = CGRectGetMaxY(self.bounds)
+        for i in 1 ..< self.horizontalDivisions {
+            let x = horizSpecing * CGFloat(i)
+            path.moveToPoint(CGPointMake(x, minY))
+            path.addLineToPoint(CGPointMake(x, maxY))
+        }
+        return path
+    }
+
+    func axesPath() -> UIBezierPath {
+        let path = UIBezierPath()
+        path.moveToPoint(CGPointMake(CGRectGetMidX(bounds), CGRectGetMinY(bounds)))
+        path.addLineToPoint(CGPointMake(CGRectGetMidX(bounds), CGRectGetMaxY(bounds)))
+
+        path.moveToPoint(CGPointMake(CGRectGetMinX(bounds), CGRectGetMidY(bounds)))
+        path.addLineToPoint(CGPointMake(CGRectGetMaxX(bounds), CGRectGetMidY(bounds)))
+
+        path.lineWidth = 10
+        
+        return path
+    }
 }
 
 class SignalLayer : CAShapeLayer {
-  var waveform: Waveform? {
-    didSet {
-      self.updatePath()
+    var waveform: Waveform? {
+        didSet {
+            self.updatePath()
+        }
     }
-  }
-
-  var yScale: CGFloat = 1.0
-
-  func updatePath() {
-    let path = self.waveform?.path.copy() as UIBezierPath
-    path.applyTransform(CGAffineTransformMakeScale(1, -self.yScale))
-    self.path = path.CGPath
-  }
-
-  func setup() {
-    self.lineJoin = kCALineCapRound
-    self.lineCap = kCALineCapRound
-    self.lineWidth = 1.5
-    self.strokeColor = UIColor.whiteColor().CGColor
-  }
-
-  override init() {
-    super.init()
-    self.setup()
-  }
-
-  required init(coder: NSCoder) {
-    super.init(coder: coder)
-    self.setup()
-  }
-  
-  override init(layer: AnyObject!) {
-    super.init(layer: layer)
-  }
+    
+    var yScale: CGFloat = 1.0
+    
+    func updatePath() {
+        let path = self.waveform?.path.copy() as UIBezierPath
+        path.applyTransform(CGAffineTransformMakeScale(1, -self.yScale))
+        self.path = path.CGPath
+    }
+    
+    func setup() {
+        self.lineJoin = kCALineCapRound
+        self.lineCap = kCALineCapRound
+        self.lineWidth = 1.5
+        self.strokeColor = UIColor.whiteColor().CGColor
+    }
+    
+    override init() {
+        super.init()
+        self.setup()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setup()
+    }
+    
+    override init(layer: AnyObject!) {
+        super.init(layer: layer)
+    }
 }
